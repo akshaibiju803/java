51f27f9ce5ec467b3864e91601ee342e560d15bb
akshaibiju803 <Akshaibiju803@gmail.com>
2025-03-13 10:48:59 +0530

    double to int

---
+++ mydouble.java
@@ -0,0 +1,7 @@
+public class mydouble {
+    public static void main(String[] args) {
+        double mydouble = 10.5;
+        int myint = (int)mydouble;
+        System.out.println(myint);   
+    }
+}
