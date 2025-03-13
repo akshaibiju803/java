d02ea679bd60886f503cb641e8f7d9c971a8d79e
akshaibiju803 <Akshaibiju803@gmail.com>
2025-03-13 11:13:25 +0530

    operators

---
+++ arithameric.java
@@ -0,0 +1,11 @@
+public class arithameric {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
---
+++ assignment.java
@@ -0,0 +1,8 @@
+public class assignment {
+    public static void main(String[] args) {
+        int num = 22;
+        int num2 = num;
+        System.out.println(num2);
+        System.out.println(num);   
+    }
+}
