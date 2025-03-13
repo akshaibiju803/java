70a3e99c9855741e6aa76cd6aef442fd629acf90
akshaibiju803 <Akshaibiju803@gmail.com>
2025-03-13 12:11:08 +0530

    logical operator

---
+++ logical.java
@@ -0,0 +1,19 @@
+public class logical {
+    public static void main(String[] args) {
+        int x=10;
+        int y=20;
+
+        if(x>15 && y>15){
+            System.out.println(" AND condition TRUE ");
+        }
+        else{
+            System.out.println("AND condition FALSE ");
+        }
+        if(x>15 || y>15){
+            System.out.println("OR condition TRUE ");
+        }
+        else{
+            System.out.println("OR condition FALSE ");
+        }
+    }
+}
