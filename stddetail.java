b599ae241210ff74db6239bb24132f9e301fbc5b
akshaibiju803 <Akshaibiju803@gmail.com>
2025-03-13 09:55:16 +0530

    student details java

---
+++ stddetail.java
@@ -0,0 +1,12 @@
+public class stddetail {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        String clas = "CSE A";  
+        String Grade = "90%";
+        int marks = 70;
+        System.out.println("Name : " + name);
+        System.out.println("class: " + clas);
+        System.out.println("marks: " + marks);
+        System.out.println("Grade: " + Grade);
+    }
+}
