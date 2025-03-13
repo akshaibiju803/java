fe9c85f109a67041ead5b600688fc8535f34968a
akshaibiju803 <Akshaibiju803@gmail.com>
2025-03-13 10:46:26 +0530

    concatinate

---
+++ concat.java
@@ -0,0 +1,7 @@
+public class concat {
+    public static void main(String[] args) {
+        String mystring = "Hello";
+        String mystring2 = "World";
+        System.out.println(mystring.concat(mystring2));   
+    }
+}
